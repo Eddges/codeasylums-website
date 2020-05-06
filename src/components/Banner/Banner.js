9ef82c3d40307c0ca3588a60e50ec45b93d898b0
nagendra0032 <nagendrakumar0032@gmail.com>
2020-05-06 13:21:34 +0530

    Banner is added after curriculum section and master footer is removed

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Banner.css';
+
+
+const Banner = () =>{
+    return(
+        <div>
+        <div class="wrapper">
+        <div class="banner">
+            <div class="left">
+               <h2> We'd love to hear from you info@codeasylums.com</h2>
+            </div>
+            <div class="right">
+                <button class="button">Talk to us</button>
+                <button class="button">Explore more</button>
+            </div>
+        </div>
+    </div>
+        </div>
+    )
+}
+
+export default Banner;
