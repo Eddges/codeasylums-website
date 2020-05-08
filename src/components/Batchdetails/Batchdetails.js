471007a051d42ae668d9e3c800cadefd60f18c72
nagendra0032 <nagendrakumar0032@gmail.com>
2020-05-08 18:29:10 +0530

    Batch details added

--- src/components/Batchdetails/Batchdetails.js
+++ src/components/Batchdetails/Batchdetails.js
@@ -89,26 +89,7 @@ const Batchdetails = () => {
                 <p>200/200 tickets sold</p>
             </div>
         </div>
-        {/* <!-- 5 --> */}
-        {/* <div className="box">
-            <div className="first_section">
-                <div className="left">
-                    <h1>19</h1>
-                </div>
-                <div className="right">
-                    <p>February</p>
-                    <p>18:00 - 22:00</p>
-                </div>
-            </div>
-            <div className="second_section">
-                <h4>Machine Learning Lesson #43</h4>
-            </div>
-            <div className="divider"></div>
-            <div className="third_section">
-                <p>Booking Ended</p>
-                <p>200/200 tickets sold</p>
-            </div>
-        </div> */}
+        
     </div>
         </div>
     )
