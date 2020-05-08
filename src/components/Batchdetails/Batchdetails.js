86ae19eaa7601543655390567e82a09b03bd213d
nagendra0032 <nagendrakumar0032@gmail.com>
2020-05-08 19:40:42 +0530

    Batch section now generic

--- src/components/Batchdetails/Batchdetails.js
+++ src/components/Batchdetails/Batchdetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Batchdetails.css';
+import Box from './Box';
 
 const Batchdetails = () => {
     return(
@@ -8,87 +9,16 @@ const Batchdetails = () => {
                 <h1>Upcoming Batches</h1>
             </div>
             <div className="wrapper">
-        <div className="box">
-            <div className="first_section">
-                <div className="left">
-                    <h1>19</h1>
-                </div>
-                <div className="right">
-                    <p>February</p>
-                    <p>18:00 - 22:00</p>
-                </div>
-            </div>
-            <div className="second_section">
-                <h4>Machine Learning Lesson #43</h4>
-            </div>
-            <div className="divider"></div>
-            <div className="third_section">
-                <p>Booking Ended</p>
-                <p>200/200 tickets sold</p>
-            </div>
-        </div>
-
+        
+        
+        <Box date = "19" month = "February" time="18:00-22:00" coursename = "Machine Learning Lessson #43" status = "Booking Ended" count = "200/200tickets sold"/>
         {/* <!-- 2 --> */}
-        <div className="box">
-            <div className="first_section">
-                <div className="left">
-                    <h1>19</h1>
-                </div>
-                <div className="right">
-                    <p>February</p>
-                    <p>18:00 - 22:00</p>
-                </div>
-            </div>
-            <div className="second_section">
-                <h4>Machine Learning Lesson #43</h4>
-            </div>
-            <div className="divider"></div>
-            <div className="third_section">
-                <p>Booking Ended</p>
-                <p>200/200 tickets sold</p>
-            </div>
-        </div>
-
+        <Box date = "19" month = "February" time="18:00-22:00" coursename = "Machine Learning Lessson #43" status = "Booking Ended" count = "200/200tickets sold"/>
         {/* <!-- 3 --> */}
-        <div className="box">
-            <div className="first_section">
-                <div className="left">
-                    <h1>19</h1>
-                </div>
-                <div className="right">
-                    <p>February</p>
-                    <p>18:00 - 22:00</p>
-                </div>
-            </div>
-            <div className="second_section">
-                <h4>Machine Learning Lesson #43</h4>
-            </div>
-            <div className="divider"></div>
-            <div className="third_section">
-                <p>Booking Ended</p>
-                <p>200/200 tickets sold</p>
-            </div>
-        </div>
+        <Box date = "19" month = "February" time="18:00-22:00" coursename = "Machine Learning Lessson #43" status = "Booking Ended" count = "200/200tickets sold"/>
         {/* <!-- 4 --> */}
-        <div className="box">
-            <div className="first_section">
-                <div className="left">
-                    <h1>19</h1>
-                </div>
-                <div className="right">
-                    <p>February</p>
-                    <p>18:00 - 22:00</p>
-                </div>
-            </div>
-            <div className="second_section">
-                <h4>Machine Learning Lesson #43</h4>
-            </div>
-            <div className="divider"></div>
-            <div className="third_section">
-                <p>Booking Ended</p>
-                <p>200/200 tickets sold</p>
-            </div>
-        </div>
+        <Box date = "19" month = "February" time="18:00-22:00" coursename = "Machine Learning Lessson #43" status = "Booking Ended" count = "200/200tickets sold"/>
+
         
     </div>
         </div>
