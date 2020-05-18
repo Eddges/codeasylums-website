66d7686bba7fc7718038d15c83c2d8420e7bc1b0
Saurav <saurav@ayopop.com>
2020-05-18 20:42:45 +0530

    initial added

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -86,6 +86,9 @@ function Modalpop(){
   else{
       axios.get(`http://localhost:4001/api/saveLead?fname=${fname}&lname=${lname}&email=${email}&phone=${phone}&course=${course}&exp=${exp}`)
       .then((response, reject) => {
+            alert("Thanks, Will get back to you soon.");
+            setModalIsOpen(false);
+            console.log(response);
       })
   }
   }
