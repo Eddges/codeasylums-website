50bc8f12eb7bca92f4c026666d7dc11930535028
Eddges <shivamshekhar.i7@gmail.com>
2020-05-23 22:17:43 +0530

    important changes

--- src/pages/CourseDSA/courseDSA.js
+++ src/pages/CourseDSA/courseDSA.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
 import Navbar from '../../components/navbarCourses/Navbar'
-import MentorsDSA from '../../components/mentorsPage2/MentorsDSA/MentorsDSA'
+// import MentorsDSA from '../../components/mentorsPage2/MentorsDSA/MentorsDSA'
 import Footer from '../../components/Footer_Section/Footer'
 import MenuModal from '../../components/MenuModal/MenuModal'
 import Accordion from '../../components/accordion/Accordion'
 import Page2Top from '../../components/Page2Top/Page2Top'
-import Parallax1 from '../../components/Parallax1/Parallax1'
+// import Parallax1 from '../../components/Parallax1/Parallax1'
 import Banner from '../../components/Banner/Banner';
 import dsaBG from '../../assets/dsa.jpg'
 import BGClick from '../../components/BGClick/BGClick'
@@ -15,8 +15,8 @@ import FeedbackSliderDSA from '../../components/FeedbackSlider/FeedbackSliderDSA
 import BatchdetailsDSA from '../../components/Batchdetails/BatchDetailsDSA/BatchdetailsDSA'
 import JobReadyDSA from '../../components/JobReady/JobReadyDSA/JobReadyDSA'
 import CodeAsylumsAdvantageDSA from '../../components/CodeAsylumsAdvantage/CodeAsylumsAdvantageDSA/CodeAsylumsAdvantageDSA'
-import StudentCarouselDSA from '../../components/StudentCarousel/StudentCarouselDSA/StudentCarouselDSA'
-import BubblesDSA from '../../components/Bubbles/BubblesDSA/BubblesDSA'
+// import StudentCarouselDSA from '../../components/StudentCarousel/StudentCarouselDSA/StudentCarouselDSA'
+// import BubblesDSA from '../../components/Bubbles/BubblesDSA/BubblesDSA'
 import ReactPixel from 'react-facebook-pixel';
 import TagManager from 'react-gtm-module'
 
